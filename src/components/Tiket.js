099b2269e5dd0dfbc4e3eacea47060d3763c0bc3
alfitra <rahmatalfitra17@gmail.com>
2022-10-27 06:47:25 -0700

    Add useRef

--- src/components/Tiket.js
+++ src/components/Tiket.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FcOk } from "react-icons/fc";
 import { BiArrowToBottom } from "react-icons/bi";
